b96f48343d3f25883f105442e410f02935c16ae6
minodoragradinariu <minodoragradinariu@yahoo.ro>
2024-10-26 19:33:55 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+// Crearea și randarea galeriei
+const gallery = document.querySelector('.gallery');
+
+const markup = galleryItems.map(({ preview, original, description }) => {
+    return `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </li>
+    `;
+}).join('');
+
+gallery.insertAdjacentHTML('beforeend', markup);
+
+// Delegarea pentru a obține URL-ul imaginii mari
+gallery.addEventListener('click', (event) => {
+    event.preventDefault(); // Previne redirecționarea implicită
+
+    const target = event.target;
+    
+    if (target.nodeName === 'IMG') {
+        const originalImageURL = target.dataset.source;
+
+        // Deschiderea ferestrei modale
+        const instance = basicLightbox.create(`
+            <div class="modal">
+                <img src="${originalImageURL}" alt="${target.alt}">
+            </div>
+        `);
+
+        instance.show();
+
+        // Închiderea ferestrei modale cu tasta Escape
+        document.addEventListener('keydown', (event) => {
+            if (event.key === 'Escape') {
+                instance.close();
+            }
+        });
+    }
+});
